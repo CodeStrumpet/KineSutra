c3cbd2ab7dc784df9f1ab86424a72d47f8befc52
Christina <christinasc@gmail.com>
2012-11-03 18:49:04 -0700

    Added Arduino code

---
+++ Arduino/KinectSutra_sketch_nov03a/KinectSutra_sketch_nov03a.ino
@@ -0,0 +1,35 @@
+#define PCUART Serial
+#define PCBAUD 115200
+#define NUM_BUZZERS 3
+
+const int pins[NUM_BUZZERS] = {13, 14, 15};
+const int buzz_duration = 100; //in ms
+
+unsigned long timers[NUM_BUZZERS];
+
+char buzzer;
+
+
+void setup() {
+   PCUART.begin(PCBAUD);  
+   
+   for (int i=0;i<NUM_BUZZERS;i++) {
+     pinMode(pins[i],OUTPUT);
+     digitalWrite(pins[i],LOW);
+     timers[i] = millis();
+   }
+     
+}
+
+void loop()
+{
+  while (Serial.available() > 0) {
+    buzzer = (char)Serial.read() - 65;  //"A" will be a 0, B=1, etc...
+    timers[buzzer] = millis() + buzz_duration;
+    digitalWrite(pins[buzzer],HIGH);
+  } 
+   for (int i=0;i<NUM_BUZZERS;i++) {
+     if (timers[i]<millis()) //this buzzer's time is up, so turn it off
+       digitalWrite(pins[i],LOW);
+   }  
+}
